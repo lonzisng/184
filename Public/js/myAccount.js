43e62b0f7290096248df2ca5b5c15a22913d96a8
YWXH <1104706248@qq.com>
2019-04-28 18:04:43 +0800

    wxh 1.2.0

---
+++ Public/js/myAccount.js
@@ -0,0 +1,14 @@
+/**
+ * @Description:
+ * @author WXH
+ * @date 2019/4/28 11:25
+ */
+$("#updatePwd").click(function () {
+    console.log("222");
+    $(".updatePwd").show();
+
+    $("#update").click(function () {
+        console.log("333");
+        $(".updatePwd").hide();
+    });
+});
